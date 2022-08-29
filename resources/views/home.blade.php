4f3be530e6fa51309eb0ad0a4b95a2bf34f3d865
kaijie26 <chenkaijie26@gmail.com>
2022-08-29 16:55:31 +0200

    migrate columns

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,14 @@
-ciao
+{{-- 
+    
+    compapania - varchar (25)
+    prezzo - decimal(9,2)
+    giorni - smallint - uns
+    notti - smallint - uns
+    disponibilità - tinyint
+    data_partenza - date
+    data_ritorno - date
+    alloggio varchar (25)
+    colazione - tinyint 
+    descrizione - text - nullable
+
+    --}}
